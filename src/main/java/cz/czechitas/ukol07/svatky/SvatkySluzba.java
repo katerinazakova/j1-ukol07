9a10cc3779efd196a918e0c7c224748566b1254f
7ZKaty <zakova.kz@gmail.com>
2023-04-26 09:19:33 +0200

    Úkol sedm - Odstranění nepotřebných importů.

--- src/main/java/cz/czechitas/ukol07/svatky/SvatkySluzba.java
+++ src/main/java/cz/czechitas/ukol07/svatky/SvatkySluzba.java
@@ -1,19 +1,12 @@
 package cz.czechitas.ukol07.svatky;
 
-import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.datatype.jsr310.deser.MonthDayDeserializer;
-
 import java.io.IOException;
 import java.nio.file.Path;
 import java.time.MonthDay;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class SvatkySluzba {
 
